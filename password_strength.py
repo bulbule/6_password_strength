55f2bbd048f25d2cac68eead212a61cd30b25bae
bulbule <anyagoremykina@gmail.com>
2017-03-07 23:56:54 +0100

    collect strength points, include more years to match

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,18 @@
 import os
 import re
+import argparse
+from getpass import getpass
+
+strength_points = {'length': 3,
+                   'date': -2,
+                   'digits': 2,
+                   'sp_chars': 2,
+                   'letters': 1,
+                   'case_sense': 1,
+                   }
 
 
-def load_bad_passwords(filepath):
+def load_blacklist(filepath):
 
     if not os.path.exists(filepath):
         return None
@@ -10,19 +20,19 @@ def load_bad_passwords(filepath):
         return data_file.read()
 
 
-def check_dates(password):  # matches years 1900-2099
+def check_dates(password):
 
     indicator = 0
     yymmdd = re.compile(
-        '(19|20)\d\d(-| |/|.|_)?(0[1-9]|1[012])(-| |/|.|_)?(0[1-9]|[12][0-9]|3[01])')
+        '(1[0-9]|20)\d\d(-| |/|.|_)?(0[1-9]|1[012])(-| |/|.|_)?(0[1-9]|[12][0-9]|3[01])')
     mmddyy = re.compile(
-        '(0[1-9]|[1-9]|1[012])(-| |/|.|_)?(0[1-9]|[12][0-9]|3[01])(-| |/|.|_)?(19|20)\d\d')
+        '(0[1-9]|[1-9]|1[012])(-| |/|.|_)?(0[1-9]|[12][0-9]|3[01])(-| |/|.|_)?(1[0-9]|20)\d\d')
     ddmmyy = re.compile(
-        '(0[1-9]|[1-9]|[12][0-9]|3[01])(-| |/|.|_)?(0[1-9]|1[012])(-| |/|.|_)?(19|20)\d\d')
+        '(0[1-9]|[1-9]|[12][0-9]|3[01])(-| |/|.|_)?(0[1-9]|1[012])(-| |/|.|_)?(1[0-9]|20)\d\d')
 
-    if (len(re.findall(yymmdd, password)) >= 1) or (len(re.findall(
-            mmddyy, password)) >= 1) or (len(re.findall(ddmmyy, password)) >= 1):
-        indicator -= 2
+    if re.search(yymmdd, password) or re.search(
+            mmddyy, password) or re.search(ddmmyy, password):
+        indicator += strength_points['date']
 
     return indicator
 
@@ -33,11 +43,11 @@ def check_length(password):
     indicator = 0
 
     if 0.5 * optimal_length <= len(password) < 0.7 * optimal_length:
-        indicator += 1
+        indicator += strength_points['length'] // 3
     elif 0.7 * optimal_length < len(password) < optimal_length:
-        indicator += 2
+        indicator += 2 * strength_points['length'] // 3
     elif len(password) >= optimal_length:
-        indicator += 3
+        indicator += strength_points['length']
 
     return indicator
 
@@ -46,10 +56,10 @@ def check_digits(password):
 
     indicator = 0
 
-    if len(re.findall('\d', password)) == 1:
-        indicator += 1
-    elif len(re.findall('\d', password)) > 1:
-        indicator += 2
+    if re.search('\d', password):
+        indicator += strength_points['digits'] // 2
+        if len(re.findall('\d', password)) > 1:
+            indicator += strength_points['digits'] // 2
 
     return indicator
 
@@ -58,10 +68,10 @@ def check_special_characters(password):
 
     indicator = 0
 
-    if len(re.findall('[^a-zA-Z0-9]', password)) == 1:
-        indicator += 1
-    elif len(re.findall('[^a-zA-Z0-9]', password)) > 1:
-        indicator += 2
+    if re.search('[^a-zA-Z0-9]', password):
+        indicator += strength_points['sp_chars'] // 2
+        if len(re.findall('[^a-zA-Z0-9]', password)) > 1:
+            indicator += strength_points['sp_chars'] // 2
 
     return indicator
 
@@ -70,8 +80,8 @@ def check_letters(password):
 
     indicator = 0
 
-    if len(re.findall('[a-zA-Z]', password)) > 0:
-        indicator += 1
+    if re.search('[a-zA-Z]', password):
+        indicator += strength_points['letters']
 
     return indicator
 
@@ -80,14 +90,8 @@ def check_case_sensitivity(password):
 
     indicator = 0
 
-    if len(
-        re.findall(
-            '[a-z]',
-            password)) > 0 and len(
-            re.findall(
-                '[A-Z]',
-                password)) > 0:
-        indicator += 1
+    if re.search('[a-z]', password) and re.search('[A-Z]', password):
+        indicator += strength_points['case_sense']
 
     return indicator
 
@@ -100,21 +104,26 @@ def get_password_strength(password, blacklist):
         pass
     else:
         strength += (check_letters(password)
-                    + check_digits(password)
-                    + check_length(password)
-                    + check_special_characters(password)
-                    + check_case_sensitivity(password)
-                    + check_dates(password))
+                     + check_digits(password)
+                     + check_length(password)
+                     + check_special_characters(password)
+                     + check_case_sensitivity(password)
+                     + check_dates(password))
 
     return strength
 
 
 if __name__ == '__main__':
 
-    password = input("Enter a password:")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_blacklist')
+    args = parser.parse_args()
+    blacklist = load_blacklist(args.path_to_blacklist)
+
+    password = getpass("Enter a password:")
     if not password:
         raise ValueError('You did not enter anything.')
     else:
-        blacklist = load_bad_passwords('blacklist.txt')
+        blacklist = load_blacklist(args.path_to_blacklist)
         print('''Password's strength is {}/10'''
               .format(get_password_strength(password, blacklist)))
